aaf4519a832ef5cb397f3280aca35460cc14bbdd
Nascent <nasko6670@abv.bg>
2024-11-16 13:35:58 +0200

    Add proptypes for TodoClearCompleted

--- src/components/TodoClearCompleted.jsx
+++ src/components/TodoClearCompleted.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+TodoClearCompleted.propTypes = {
+  clearCompleted: PropTypes.func.isRequired,
+};
+
 function TodoClearCompleted(props) {
   return (
     <button onClick={props.clearCompleted} className='button'>
